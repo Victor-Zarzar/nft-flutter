dec8320f0d31845633bbe3165220ebd5b33dfaf4
victorzarzar <victorzarzar58@gmail.com>
2024-01-25 11:46:36 -0300

    first commit

---
+++ lib/components/app_theme.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+class AppTheme {
+  AppTheme._();
+
+  static Color get primaryColor => const Color(0xFF343541);
+
+  static Color get secondaryColor => const Color(0xFF40414f);
+}
+
+class Navbar {
+  Navbar._();
+
+  static Color get primaryColor => const Color(0xFFD50000);
+}
+
+class BackGround {
+  BackGround._();
+
+  static Color get primaryColor => const Color(0xFF000000);
+}
+
+class TextFormColor {
+  TextFormColor._();
+
+  static Color get primaryColor => const Color(0xFFF5F5F5);
+
+  static Color get secondaryColor => const Color(0xFF000000);
+}
+
+class Button {
+  Button._();
+
+  static Color get primaryColor => const Color(0xFFD50000);
+
+  static Color get secondaryColor => const Color(0xFFF44336);
+}

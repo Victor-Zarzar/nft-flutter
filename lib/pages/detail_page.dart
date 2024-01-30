08114d3892049b65bc2d1ad1e673a58c8fce63b1
victorzarzar <victorzarzar58@gmail.com>
2024-01-29 22:16:33 -0300

    create page detailPage and updates in post page, and defined variables in detailPage

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,33 @@
+import "package:flutter/material.dart";
+import "package:nft_flutter/components/app_theme.dart";
+
+class DetailPage extends StatelessWidget {
+  final String price;
+  final String creator;
+  final String image;
+  final String minutes;
+  final String hours;
+  final String day;
+  final String name;
+  final String creatorImage;
+  final String follower;
+  const DetailPage({
+    super.key,
+    required this.price,
+    required this.creator,
+    required this.image,
+    required this.minutes,
+    required this.hours,
+    required this.day,
+    required this.name,
+    required this.creatorImage,
+    required this.follower,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: BackGround.primaryColor,
+    );
+  }
+}
--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -1,6 +1,7 @@
 import "dart:ui";
 import "package:flutter/material.dart";
 import "package:nft_flutter/components/app_theme.dart";
+import "package:nft_flutter/pages/detail_page.dart";
 
 class PostPage extends StatefulWidget {
   final String price;
@@ -35,165 +36,130 @@ class _PostPageState extends State<PostPage> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('lib/assets/${widget.image}.png'),
-            fit: BoxFit.cover,
-          ),
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              offset: const Offset(0, 5),
-              color: PostColor.primaryColor,
-              blurRadius: 16,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => DetailPage(
+              creator: widget.creator,
+              name: widget.name,
+              price: widget.price,
+              image: widget.image,
+              day: widget.day,
+              follower: widget.follower,
+              creatorImage: widget.creatorImage,
+              minutes: widget.minutes,
+              hours: widget.hours,
             ),
-          ],
-        ),
-        width: double.infinity,
-        height: 350,
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 15),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(5),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 36,
-                        height: 36,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              Colors.grey.shade200.withOpacity(0.6),
-                              Colors.grey.shade200.withOpacity(0.3),
-                            ],
-                          ),
-                        ),
-                        child: Text(
-                          '${widget.day} D',
-                          style: const TextStyle(fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 4),
-                  const Text(
-                    ':',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(5),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 36,
-                        height: 36,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              CardColor.primaryColor.withOpacity(0.6),
-                              CardColor.primaryColor.withOpacity(0.3),
-                            ],
-                          ),
-                        ),
-                        child: Text(
-                          '${widget.hours} H',
-                          style: const TextStyle(fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 4),
-                  const Text(
-                    ':',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(5),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 36,
-                        height: 36,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              CardColor.primaryColor.withOpacity(0.6),
-                              CardColor.primaryColor.withOpacity(0.3),
-                            ],
-                          ),
-                        ),
-                        child: Text(
-                          '${widget.minutes} M',
-                          style: const TextStyle(fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+          ));
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('lib/assets/${widget.image}.png'),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(0, 5),
+                color: PostColor.primaryColor,
+                blurRadius: 16,
               ),
-            ),
-            Positioned(
-              top: 270,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
+            ],
+          ),
+          width: double.infinity,
+          height: 350,
+          child: Stack(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 14, vertical: 15),
                 child: Row(
                   children: [
-                    Expanded(flex: 3, child: Container(height: 50)),
-                    Expanded(
-                      flex: 2,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(5),
-                        child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                          child: Container(
-                            height: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              gradient: LinearGradient(
-                                begin: Alignment.centerLeft,
-                                end: Alignment.centerRight,
-                                colors: [
-                                  CardColor.primaryColor.withOpacity(0.6),
-                                  CardColor.primaryColor.withOpacity(0.3),
-                                ],
-                              ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 36,
+                          height: 36,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Colors.grey.shade200.withOpacity(0.6),
+                                Colors.grey.shade200.withOpacity(0.3),
+                              ],
                             ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 8),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Image.asset('lib/assets/et.png'),
-                                  Text(
-                                    '${widget.price} ETH',
-                                    style: const TextStyle(
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                ],
-                              ),
+                          ),
+                          child: Text(
+                            '${widget.day} D',
+                            style: const TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    const Text(
+                      ':',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 36,
+                          height: 36,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                CardColor.primaryColor.withOpacity(0.6),
+                                CardColor.primaryColor.withOpacity(0.3),
+                              ],
                             ),
+                          ),
+                          child: Text(
+                            '${widget.hours} H',
+                            style: const TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    const Text(
+                      ':',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 36,
+                          height: 36,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                CardColor.primaryColor.withOpacity(0.6),
+                                CardColor.primaryColor.withOpacity(0.3),
+                              ],
+                            ),
+                          ),
+                          child: Text(
+                            '${widget.minutes} M',
+                            style: const TextStyle(fontWeight: FontWeight.w600),
                           ),
                         ),
                       ),
@@ -201,8 +167,62 @@ class _PostPageState extends State<PostPage> {
                   ],
                 ),
               ),
-            ),
-          ],
+              Positioned(
+                top: 270,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    children: [
+                      Expanded(flex: 3, child: Container(height: 50)),
+                      Expanded(
+                        flex: 2,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                gradient: LinearGradient(
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                  colors: [
+                                    CardColor.primaryColor.withOpacity(0.6),
+                                    CardColor.primaryColor.withOpacity(0.3),
+                                  ],
+                                ),
+                              ),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Image.asset('lib/assets/et.png'),
+                                    Text(
+                                      '${widget.price} ETH',
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
